c93b1ae0beeb09c3e4ed5a1f4e75b8820dda31b0
Serrones <fabioserrones@gmail.com>
2018-07-09 18:06:43 -0300

    Inscrição-Retorna E-mail

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
@@ -41,3 +42,65 @@ class SubscriptionTest(TestCase):
         form = self.resp.context['form']
         self.assertSequenceEqual(['nome', 'cpf', 'email', 'fone'],
                                   list(form.fields))
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(nome="Fábio Serrão", cpf="12345678901",
+                    email="fabioserrones@gmail.com", fone="11-99526-3577")
+        self.resp = self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_post(self):
+        """Valid post should redirect to /inscricao/"""
+        self.assertEqual(302, self.resp.status_code)
+
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+        self.assertEqual(expect, self.email.subject)
+
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(expect, self.email.from_email)
+
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'fabioserrones@gmail.com']
+        self.assertEqual(expect, self.email.to)
+
+
+    def test_subscription_email_body(self):
+        self.assertIn('Fábio Serrão', self.email.body)
+        self.assertIn('12345678901', self.email.body)
+        self.assertIn('fabioserrones@gmail.com', self.email.body)
+        self.assertIn('11-99526-3577', self.email.body)
+
+
+class SubscribeInvalidPost(TestCase):
+    def setUp(self):
+        self.resp = self.client.post('/inscricao/', data={})
+
+
+    def test_post(self):
+        """Invalid POST should not redirect"""
+        self.assertEqual(200, self.resp.status_code)
+
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+
+
+    def test_has_form(self):
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+
+    def test_form_has_errors(self):
+        form = self.resp.context['form']
+        self.assertTrue(form.errors)
